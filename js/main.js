32415bb58f9f68b5e26a5a0c7e9a25ad082ed444
theeyad <eyadosman35@gmail.com>
2026-04-20 15:07:17 +0200

    starting quizes

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// Get Questions Number For Each Card
+// Set Questions Number For Each Card
 const testCards = document.querySelectorAll(".card");
 const testNumSpans = document.querySelectorAll(".num-of-questions");
 
@@ -22,8 +22,16 @@ const cardsGrid = document.querySelector(".cards");
 const quizBox = document.querySelector(".test-box");
 
 testCards.forEach((card) =>
-  card.addEventListener("click", (e) => {
+  card.addEventListener("click", () => {
     cardsGrid.classList.add("hidden");
     quizBox.classList.remove("hidden");
+
+    startQuiz(card.classList[1]);
   }),
 );
+
+// Load Quiz
+function startQuiz(topic) {
+  let topicName = document.querySelector(".topic-name");
+  topicName.innerHTML = topic;
+}
