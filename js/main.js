8fc09d7c9c398939b1fe944ad39c7ecda201e518
theeyad <eyadosman35@gmail.com>
2026-04-21 23:02:48 +0200

    fixing functions and finallizing

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+// Global Vars
+let jsonData = [];
+let quizTimer;
+let currentQuestion = 0;
+let correctAnswers = 0;
+let wrongAnswers = 0;
+
+const answers = document.querySelectorAll(".answers .answer");
+const resultBox = document.querySelector(".result-box");
+
 // Set Questions Number For Each Card
 const testCards = document.querySelectorAll(".card");
 const testNumSpans = document.querySelectorAll(".num-of-questions");
@@ -26,42 +36,48 @@ testCards.forEach((card) =>
     cardsGrid.classList.add("hidden");
     quizBox.classList.remove("hidden");
 
+    // Name of the Topic
     startQuiz(card.classList[1]);
   }),
 );
 
 // Load Quiz
 async function startQuiz(topic) {
+  // resetting everything
+  correctAnswers = 0;
+  wrongAnswers = 0;
+  currentQuestion = 0;
+
   let topicName = document.querySelector(".topic-name");
   topicName.innerHTML = topic;
 
   let result = await fetch(`questions/${topic}_questions.json`);
   let data = await result.json();
+  jsonData = data;
 
-  createQuizBody(data);
+  createTimer();
 }
 
 // Make Timer
-function createTimer(numOfQuestions, correctAnswers, wrongAnswers) {
+function createTimer() {
   const quizStarted = Date.now();
   const testTime = document.querySelector(".test-time");
 
-  const numOfMinutes = numOfQuestions / 2;
+  const numOfMinutes = 0.5;
   const minuteInMs = numOfMinutes * 60 * 1000;
 
   testTime.textContent = `${String(numOfMinutes).padStart(2, "0")}:00`;
 
-  const timerInterval = setInterval(() => {
+  quizTimer = setInterval(() => {
     let current = Date.now();
     let passed = current - quizStarted;
 
     const remainingMs = minuteInMs - passed;
 
-    // 1. Check if time is up!
     if (remainingMs <= 0) {
-      clearInterval(timerInterval);
+      clearInterval(quizTimer);
       testTime.textContent = "00:00";
-      showResult(correctAnswers, wrongAnswers, numOfQuestions);
+      showResult(correctAnswers, wrongAnswers, jsonData.length);
       return;
     }
 
@@ -74,102 +90,24 @@ function createTimer(numOfQuestions, correctAnswers, wrongAnswers) {
     const formattedSeconds = String(seconds).padStart(2, "0");
 
     testTime.textContent = `${formattedMinutes}:${formattedSeconds}`;
-
-    console.log(passed)
-
-    if (passed >= minuteInMs) {
-      console.log("done");
-      clearInterval(timerInterval);
-      showResult(correctAnswers, wrongAnswers, numOfQuestions);
-      return;
-    }
   }, 1000);
+
+  createQuizBody();
 }
 
 // Create Question and Chices
-function createQuizBody(data) {
-  let correctAnswers = 0;
-  let wrongAnswers = 0;
-  let currentQuestion = 0;
-
-  createTimer(data.length, correctAnswers, wrongAnswers);
-
+function createQuizBody() {
   let question = document.querySelector(".question");
-  question.innerHTML = "";
-  question.append(document.createTextNode(data[currentQuestion].question));
+  question.textContent = `${jsonData[currentQuestion].question}`;
 
-  let answers = document.querySelectorAll(".answers .answer");
   answers.forEach((answer, key) => {
-    answer.innerHTML = "";
-    answer.append(document.createTextNode(data[currentQuestion].choices[key]));
-  });
-
-  answerListeners(answers);
-
-  document.querySelector(".submit").addEventListener("click", () => {
-    if (document.querySelector(".answers .answer.active")) {
-      if (
-        document.querySelector(".answers .answer.active").textContent ===
-        data[currentQuestion].answer
-      ) {
-        correctAnswers++;
-      } else {
-        wrongAnswers++;
-      }
-
-      document
-        .querySelector(".answers .answer.active")
-        .classList.add("border-slate-200");
-      document
-        .querySelector(".answers .answer.active")
-        .classList.remove("active", "bg-indigo-50", "border-indigo-300");
-    } else {
-      wrongAnswers++;
-    }
-
-    currentQuestion++;
-
-    if (data[currentQuestion] === undefined) {
-      showResult(correctAnswers, wrongAnswers, data.length);
-      return;
-    }
-
-    let question = document.querySelector(".question");
-    question.innerHTML = "";
-    question.append(document.createTextNode(data[currentQuestion].question));
-
-    answers.forEach((answer, key) => {
-      answer.innerHTML = "";
-      answer.append(
-        document.createTextNode(data[currentQuestion].choices[key]),
-      );
-    });
-  });
-}
-
-// Add Event Listeners to Each answer
-function answerListeners(answers) {
-  answers.forEach((answer) => {
-    answer.addEventListener("click", () => {
-      if (document.querySelector(".answers .answer.active")) {
-        document
-          .querySelector(".answers .answer.active")
-          .classList.add("border-slate-200");
-        document
-          .querySelector(".answers .answer.active")
-          .classList.remove("active", "bg-indigo-50", "border-indigo-300");
-      }
-
-      answer.classList.add("active", "bg-indigo-50", "border-indigo-300");
-      answer.classList.remove("border-slate-200");
-    });
+    answer.textContent = `${jsonData[currentQuestion].choices[key]}`;
   });
 }
 
 // Show Quiz Result
 function showResult(correctAnswers, wrongAnswers, numOfQuestions) {
   quizBox.classList.add("hidden");
-  const resultBox = document.querySelector(".result-box");
 
   resultBox.classList.remove("hidden");
 
@@ -181,16 +119,69 @@ function showResult(correctAnswers, wrongAnswers, numOfQuestions) {
   const trueAnswers = document.querySelector(".right-answers");
   const falseAnswers = document.querySelector(".wrong-answers");
 
-  const back = document.querySelector(".back");
-
   score.innerHTML = correctAnswers;
   numberOfQuestions.innerHTML = numOfQuestions;
 
   trueAnswers.innerHTML = correctAnswers;
   falseAnswers.innerHTML = wrongAnswers;
-
-  back.addEventListener("click", () => {
-    resultBox.classList.add("hidden");
-    cardsGrid.classList.remove("hidden");
-  });
 }
+
+// Event Listeners
+answers.forEach((answer) => {
+  answer.addEventListener("click", () => {
+    if (document.querySelector(".answers .answer.active")) {
+      document
+        .querySelector(".answers .answer.active")
+        .classList.add("border-slate-200");
+      document
+        .querySelector(".answers .answer.active")
+        .classList.remove("active", "bg-indigo-50", "border-indigo-300");
+    }
+
+    answer.classList.add("active", "bg-indigo-50", "border-indigo-300");
+    answer.classList.remove("border-slate-200");
+  });
+});
+
+document.querySelector(".submit").addEventListener("click", () => {
+  if (document.querySelector(".answers .answer.active")) {
+    if (
+      document.querySelector(".answers .answer.active").textContent ===
+      jsonData[currentQuestion].answer
+    ) {
+      correctAnswers++;
+    } else {
+      wrongAnswers++;
+    }
+
+    document
+      .querySelector(".answers .answer.active")
+      .classList.add("border-slate-200");
+    document
+      .querySelector(".answers .answer.active")
+      .classList.remove("active", "bg-indigo-50", "border-indigo-300");
+  } else {
+    wrongAnswers++;
+  }
+
+  currentQuestion++;
+
+  if (jsonData[currentQuestion] === undefined) {
+    clearInterval(quizTimer);
+    showResult(correctAnswers, wrongAnswers, jsonData.length);
+    return;
+  }
+
+  let question = document.querySelector(".question");
+  question.textContent = `${jsonData[currentQuestion].question}`;
+
+  answers.forEach((answer, key) => {
+    answer.textContent = `${jsonData[currentQuestion].choices[key]}`;
+  });
+});
+
+const back = document.querySelector(".back");
+back.addEventListener("click", () => {
+  resultBox.classList.add("hidden");
+  cardsGrid.classList.remove("hidden");
+});
